9bdc4b454b7812cc262818c38f43ccc43a944591
QWEDt <artem0m2@yandex.ru>
2022-10-20 19:51:37 +0500

    Added echo PCBuilderBot

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new PCBuilderBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/org/example/PCBuilderBot.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class PCBuilderBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return System.getenv("botName");
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("botToken");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            sendText(message, message.getText());
+        }
+    }
+
+    public void sendText(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
