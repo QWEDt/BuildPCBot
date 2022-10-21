e40afd534f246fcdee8dc576cc30aca65639c7e8
evgeniy <103364915+fergaf@users.noreply.github.com>
2022-10-21 11:55:16 +0500

    fill components.json

--- src/main/java/org/example/PCBuilderBot.java
+++ src/main/java/org/example/PCBuilderBot.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashMap;
+
 public class PCBuilderBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
@@ -22,6 +24,25 @@ public class PCBuilderBot extends TelegramLongPollingBot {
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
             sendText(message, message.getText());
+            switch (message.getText()){
+                case"/start":
+                    //sendText();
+                    break;
+                case "/BuildPS":
+                    //sendText();
+                    HashMap<String, String> PC;
+                    sendText(message, PC.get("GPUs"));
+                    sendText(message, PC.get("CPUs"));
+                    sendText(message, PC.get("motherboards"));
+                    sendText(message, PC.get("ram"));
+                    sendText(message, PC.get("cooling"));
+                    sendText(message, PC.get("powers"));
+                    sendText(message, PC.get("corpuses"));
+                    sendText(message, PC.get("disks"));
+                    break;
+
+
+            }
         }
     }
 
